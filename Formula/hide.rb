3763260c7be1632809310b31c1b695aaf7954d5f
Artem Moskvin <artemm@spotify.com>
2024-06-26 14:39:46 +0200

    Fix format

--- Formula/hide.rb
+++ Formula/hide.rb
@@ -1,13 +1,13 @@
 class Hide < Formula
-  desc "🤖 Headless IDE for AI agents"
+  desc "Headless IDE for AI agents"
   homepage "https://hide.sh"
   url "https://github.com/artmoskvin/hide/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "84726116073d42b0d4beb476ead493a7efe88c1c7cc8141e4b1d64054f0c30c3"
   license "MIT"
 
   depends_on "go" => :build
-  depends_on "git"
   depends_on "docker"
+  depends_on "git"
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/hide"
